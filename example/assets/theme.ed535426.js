84cad9593682a4057db601bc65f4ea08d6589fd5
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-06-08 16:12:06 -0700

    Update example

--- example/assets/theme.ed535426.js
+++
@@ -1,2 +0,0 @@
-console.log("Vite \u26A1\uFE0F Shopify");
-//# sourceMappingURL=theme.ed535426.js.map
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import shopify from 'vite-plugin-shopify'
+import Shopify from 'vite-plugin-shopify'
 
 export default defineConfig({
   build: {
     emptyOutDir: true
   },
   plugins: [
-    shopify()
+    Shopify()
   ]
 })
