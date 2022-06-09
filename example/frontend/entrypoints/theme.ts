bc0ac05d908aa5528b036e405041b5a5feb60b5f
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-06-08 17:23:14 -0700

    Update example

--- example/frontend/entrypoints/theme.ts
+++ example/frontend/entrypoints/theme.ts
@@ -1,2 +1,7 @@
 import 'vite/modulepreload-polyfill'
-console.log('Vite ⚡️ Shopify')
+
+import('lodash-es')
+  .then(({ default: _ }) => {
+    console.log(_.join(['Vite', '⚡️', 'Shopify'], ' '))
+  })
+  .catch(error => console.log(error))
