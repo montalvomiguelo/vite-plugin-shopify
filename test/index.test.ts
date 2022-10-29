f33c0c690cb98c84ea994d63ea4f1a2b29fba088
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-10-28 20:34:41 -0700

    Disable type checking for a line

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,6 +3,8 @@ import shopify from '../src/index'
 
 describe('vite-plugin-shopify', () => {
   it('handles missing configuration', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
     expect(() => shopify()).toThrow('vite-plugin-shopify: missing configuration.')
 
     expect(() => shopify({})).toThrow('vite-plugin-shopify: missing configuration for "input".')
