48deabd214cc133b1cd13751b281975ed6d50bdb
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-12-26 17:33:16 -0600

    Test that the plugin runs as expected

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,26 @@
 import { describe, expect, it } from 'vitest'
+import { build } from 'vite'
 import shopify from '../src/index'
+import fs from 'fs/promises'
 
 describe('vite-plugin-shopify', () => {
-  it('handles missing configuration', () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    expect(() => shopify()).toThrow('vite-plugin-shopify: missing configuration.')
+  it('runs as expected', async () => {
+    await build({
+      plugins: [
+        shopify({
+          input: [
+            'test/__fixtures__/resources/css/theme.css',
+            'test/__fixtures__/resources/js/theme.js'
+          ],
+          themeRoot: 'test/__fixtures__'
+        })
+      ]
+    })
 
-    expect(() => shopify({})).toThrow('vite-plugin-shopify: missing configuration for "input".')
+    const tagsHtml = await fs.readFile('test/__fixtures__/snippets/vite-tag.liquid', { encoding: 'utf8' })
+    const clientHtml = await fs.readFile('test/__fixtures__/snippets/vite-client.liquid', { encoding: 'utf8' })
+
+    expect(tagsHtml).toMatchSnapshot()
+    expect(clientHtml).toMatchSnapshot()
   })
 })
